50be05fa36c5c725a6657767e74b88546de5edb9
darchcruise <darchcruise@yahoo.com>
2013-12-08 15:27:08 -0500

    redis for heroku

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,15 @@
+if Rails.production? # (on Heroku)
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new
+end
+
 # config/initializers/geocoder.rb
 Geocoder.configure(
   :lookup => :google,
   :timeout => 30,
   :units => :km,
-  :cache => Redis.new,
+  :cache => REDIS,
   :cache_prefix => "something"
 )
